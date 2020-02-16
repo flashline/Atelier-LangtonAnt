338852f3db6532b6101fcb81cb02b73133ad5d0f
jean-michel delettre <info@pixaline.net>
2020-02-16 17:30:38 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ const toLeft 	=-90;				// anti-clockwise
 const up		=0;					// up orientation
 const down 		=180;				// down orientation
 var Driver = {}; 					// Engine driver where all parameters are.
-Driver.maxStep=-1; 					// max step number before stop or -1 for infinite
-Driver.frequency=100;					// Number of frame per second
+Driver.maxStep=4; 					// max step number before stop or -1 for infinite
+Driver.frequency=1;					// Number of frame per second
 Driver.skip=false;					// if true only some views are displayed (freeze frame effect)
 									// If false : step's number equal view's number
 Driver.skipMethod="pauseOnZero"; 	// "pauseOnZero" or "pauseOnValue" (default)
@@ -14,7 +14,7 @@ Driver.skipValue=1;					// Number of skipped steps between 2 views if skipMethod
 Driver.skipStart=0;	 				// First skip value before first view. 
 //
 Driver.initRotation=toLeft;			// Start orientation of Ant
-initColorChain=initColorChain14; 	// Choose one of the functions below
+initColorChain=initColorChain1; 	// Choose one of the functions below
 log (" "+initColorChain.toString().substr(9,16));
 //
 initColorChain();
